95d55cebdbfc39c7ce6ccc56af853b71150ab9f0
Emiliya Ileeva <emilyileeva@gmail.com>
2025-05-23 10:24:27 -0700

    Add Drink class with size, flavor, and pricing

--- src/main/java/com/nomnomdeli/model/Drink.java
+++ src/main/java/com/nomnomdeli/model/Drink.java
@@ -1,4 +1,37 @@
 package com.nomnomdeli.model;
 
 public class Drink {
+    private String size;
+    private String flavor;
+
+    public Drink(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public double getPrice() {
+        switch (size.toLowerCase()) {
+            case "small":
+                return 2.00;
+            case "medium":
+                return 2.50;
+            case "large":
+                return 3.00;
+            default:
+                return 0.00; // Invalid size
+        }
+    }
+
+    @Override
+    public String toString() {
+        return size + " " + flavor + " - $" + String.format("%.2f", getPrice());
+    }
 }
