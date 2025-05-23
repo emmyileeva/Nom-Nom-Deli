3c7b8389f87e6c02c674d7ac3424f0dbcd94c096
Emiliya Ileeva <emilyileeva@gmail.com>
2025-05-23 10:18:58 -0700

    Add Chip class with type and fixed price

--- src/main/java/com/nomnomdeli/model/Chip.java
+++ src/main/java/com/nomnomdeli/model/Chip.java
@@ -1,4 +1,23 @@
 package com.nomnomdeli.model;
 
 public class Chip {
+    private String type;
+    private static final double PRICE = 1.50;
+
+    public Chip(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return PRICE;
+    }
+
+    @Override
+    public String toString() {
+        return type + " Chips - $" + String.format("%.2f", PRICE);
+    }
 }
