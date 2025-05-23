7dae52d41a641b3edc00de0954855e53c530e86a
Emiliya Ileeva <emilyileeva@gmail.com>
2025-05-23 14:45:31 -0700

    Add MenuService with scanner and working HomeMenu integration

--- src/main/java/com/nomnomdeli/menu/MenuService.java
+++ src/main/java/com/nomnomdeli/menu/MenuService.java
@@ -1,4 +1,23 @@
 package com.nomnomdeli.menu;
 
+import java.util.Scanner;
+
 public class MenuService {
+    private Scanner scanner;
+
+    public MenuService() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    // This method starts the menu service
+    public void start() {
+        HomeMenu homeMenu = new HomeMenu(scanner);
+        boolean continueRunning = homeMenu.showHomeMenu();
+
+        while (continueRunning) {
+            // here is where I will show OrderMenu and handle new order flow
+            continueRunning = false; // For now, exit after one iteration
+        }
+        scanner.close();
+    }
 }
