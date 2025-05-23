d8d96af064b78130e8ea0429dea70e9fcf345dd3
Emiliya Ileeva <emilyileeva@gmail.com>
2025-05-23 12:03:30 -0700

    Add Meat class with size-based pricing and extra option

--- src/main/java/com/nomnomdeli/toppings/Meat.java
+++ src/main/java/com/nomnomdeli/toppings/Meat.java
@@ -1,4 +1,29 @@
 package com.nomnomdeli.toppings;
 
-public class Meat {
+public class Meat extends PremiumTopping {
+
+    public Meat(String name, boolean isExtra) {
+        super(name, isExtra);
+    }
+
+    // price of meat toppings based on size
+    @Override
+    public double getPrice(String size) {
+        double basePrice;
+        switch (size) {
+            case "4":
+                basePrice = 1.0;
+                break;
+            case "8":
+                basePrice = 2.0;
+                break;
+            case "12":
+                basePrice = 3.0;
+                break;
+            default:
+                basePrice = 0.0; // default case for invalid size
+                break;
+        }
+        return isExtra ? basePrice * 2 : basePrice; // double the price if it's extra
+    }
 }
