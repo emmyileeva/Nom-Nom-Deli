690aab0af7a00dd639618054f5a0369e6920674e
Emiliya Ileeva <emilyileeva@gmail.com>
2025-05-23 13:49:12 -0700

    Add NomNomDeliApp with main method to launch MenuService start method (not implemented yet)

--- src/main/java/com/nomnomdeli/NomNomDeliApp.java
+++ src/main/java/com/nomnomdeli/NomNomDeliApp.java
@@ -1,7 +1,11 @@
 package com.nomnomdeli;
 
+import com.nomnomdeli.menu.MenuService;
+
 public class NomNomDeliApp {
     public static void main(String[] args) {
-
+        MenuService menuService = new MenuService();
+        // Still need to create this method
+        menuService.start();
     }
 }
