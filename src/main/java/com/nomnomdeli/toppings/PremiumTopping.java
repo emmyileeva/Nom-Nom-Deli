659bc400f52d380c3ff331cc4a67c95fb013b2e5
Emiliya Ileeva <emilyileeva@gmail.com>
2025-05-23 11:52:55 -0700

    Add PremiumTopping abstract class with shared logic for name and extra for toppings

--- src/main/java/com/nomnomdeli/toppings/PremiumTopping.java
+++ src/main/java/com/nomnomdeli/toppings/PremiumTopping.java
@@ -1,4 +1,20 @@
 package com.nomnomdeli.toppings;
-// abstract class
-public class PremiumTopping {
+
+public abstract class PremiumTopping implements Topping {
+    // using protected to allow subclasses to access these fields but not outside the package
+    protected String name;
+    protected boolean isExtra;
+
+    public PremiumTopping(String name, boolean isExtra) {
+        this.name = name;
+        this.isExtra = isExtra;
+    }
+
+    @Override
+    public String getName() {
+        return name + (isExtra ? " (extra)" : "");
+    }
+
+    // getPrice method will be implemented in subclasses ( Meat, Cheese)
+    // they can call isExtra to check whether to double the price
 }
