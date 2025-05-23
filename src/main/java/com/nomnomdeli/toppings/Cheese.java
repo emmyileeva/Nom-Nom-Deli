4eca322e04f4e103dab59cec8b5c91a259c53dd3
Emiliya Ileeva <emilyileeva@gmail.com>
2025-05-23 12:37:00 -0700

    Add Cheese class and fix extra price logic in Meat to match correct pricing rules

--- src/main/java/com/nomnomdeli/toppings/Cheese.java
+++ src/main/java/com/nomnomdeli/toppings/Cheese.java
@@ -1,4 +1,34 @@
 package com.nomnomdeli.toppings;
 
-public class Cheese {
+public class Cheese extends PremiumTopping {
+
+    public Cheese(String name, boolean isExtra) {
+        super(name, isExtra);
+    }
+
+    // price of cheese toppings based on size
+    @Override
+    public double getPrice(String size) {
+        double basePrice;
+        double extraPrice = 0.0;
+
+        switch (size) {
+            case "4":
+                basePrice = 0.75;
+                extraPrice = 0.30;
+                break;
+            case "8":
+                basePrice = 1.5;
+                extraPrice = 0.60;
+                break;
+            case "12":
+                basePrice = 2.25;
+                extraPrice = 0.90;
+                break;
+            default:
+                basePrice = 0.0; // default case for invalid size
+                break;
+        }
+        return isExtra ? basePrice + extraPrice : basePrice; // add extra price if it's extra
+    }
 }
--- src/main/java/com/nomnomdeli/toppings/Meat.java
+++ src/main/java/com/nomnomdeli/toppings/Meat.java
@@ -10,20 +10,25 @@ public class Meat extends PremiumTopping {
     @Override
     public double getPrice(String size) {
         double basePrice;
+        double extraPrice = 0.0;
+
         switch (size) {
             case "4":
                 basePrice = 1.0;
+                extraPrice = 0.5;
                 break;
             case "8":
                 basePrice = 2.0;
+                extraPrice = 1.0;
                 break;
             case "12":
                 basePrice = 3.0;
+                extraPrice = 1.5;
                 break;
             default:
                 basePrice = 0.0; // default case for invalid size
                 break;
         }
-        return isExtra ? basePrice * 2 : basePrice; // double the price if it's extra
+        return isExtra ? basePrice + extraPrice : basePrice; // add extra price if it's extra
     }
 }
