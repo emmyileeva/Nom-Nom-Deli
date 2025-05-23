5087dde2c294442f2b267c5fe843d11757355851
Emiliya Ileeva <emilyileeva@gmail.com>
2025-05-23 11:34:15 -0700

    Add RegularTopping class with zero cost logic

--- src/main/java/com/nomnomdeli/toppings/RegularTopping.java
+++ src/main/java/com/nomnomdeli/toppings/RegularTopping.java
@@ -1,4 +1,19 @@
 package com.nomnomdeli.toppings;
 
-public class RegularTopping {
+public class RegularTopping implements Topping {
+    private String name;
+
+    public RegularTopping(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice(String sandwichSize) {
+        return 0.0; // Regular toppings are free
+    }
 }
