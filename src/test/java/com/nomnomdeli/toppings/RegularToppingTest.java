8fe41fd5073ee30ccf9518e11af8c45106ef6002
Emiliya Ileeva <emilyileeva@gmail.com>
2025-05-29 10:14:28 -0700

    Add unit tests for the RegularTopping class to test the name and price behavior

--- src/test/java/com/nomnomdeli/toppings/RegularToppingTest.java
+++ src/test/java/com/nomnomdeli/toppings/RegularToppingTest.java
@@ -3,13 +3,32 @@ package com.nomnomdeli.toppings;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class RegularToppingTest {
 
     @Test
-    void getName() {
+    void getName_shouldReturnTheCorrectName() {
+        // Arrange
+        RegularTopping regularTopping = new RegularTopping("Lettuce");
+
+        // Act
+        String name = regularTopping.getName();
+
+        // Assert
+        assertEquals("Lettuce", name);
     }
 
     @Test
-    void getPrice() {
+    void getPrice_shouldAlwaysReturnZeroRegardlessOfTheSize() {
+        // Arrange
+        RegularTopping regularTopping = new RegularTopping("Tomato");
+
+        // Act & Assert
+        assertEquals(0.0, regularTopping.getPrice("4"));
+        assertEquals(0.0, regularTopping.getPrice("8"));
+        assertEquals(0.0, regularTopping.getPrice("12"));
+        assertEquals(0.0, regularTopping.getPrice("InvalidSize"));
+
+
     }
 }
